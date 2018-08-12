94170ade54152c77859988179b912045b6b20d83
Mike Qiu <19714287+Theophylline@users.noreply.github.com>
2018-08-11 21:21:49 -0400

    RL agent

--- agent.py
+++ agent.py
@@ -1,35 +1,42 @@
 import numpy as np
 from collections import deque
-from keras.models import Sequential
+from keras.models import Model
 from skimage import transform
-from keras.layers import Conv2D, Flatten, Dense, BatchNormalization
+from keras.layers import Input, Conv2D, Flatten, Dense, BatchNormalization, MaxPooling2D
+from keras import optimizers
+import keras.backend as K
 import retro
 
 # HYPERPARAMETERS
-gamma = 0.98
 state_width = 84 # resized state width
 state_height = 84 # resized state height
 stack_size = 4 # how many states/frames to stack together to detect motion
 
-
 #%%
 
-def advantage(episode_rewards, gamma):
+def discount(episode_rewards, gamma):
     discounted_rewards = np.zeros_like(episode_rewards, dtype=np.float64)
+    
+    # to prevent division by 0 during normalization, which may occur if the 
+    # agent received no reward during an episode 
     sum = 0
-    idx = len(episode_rewards) - 1
     
-    for reward in reversed(episode_rewards):
-        sum = sum * gamma + reward
-        discounted_rewards[idx] = sum
-        idx = idx - 1
-    
-    # normalize the discounted_rewards vector
-    mean = discounted_rewards.mean()
-    std = discounted_rewards.std()
-    discounted_rewards = (discounted_rewards - mean) / std
+    for t in reversed(range(0, len(episode_rewards))):
+        sum = sum * gamma + episode_rewards[t]
+        discounted_rewards[t] = sum
+
+    # normalize the discounted_rewards vector; omitted due to numerical instability
+# =============================================================================
+#     mean = discounted_rewards.mean()
+#     std = discounted_rewards.std()
+#     discounted_rewards = (discounted_rewards - mean) / (std + 1e-10)
+# =============================================================================
     
     return discounted_rewards
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
 def _preprocess(state):
     # convert to grayscale from rgb
@@ -43,7 +50,12 @@ def _preprocess(state):
     grayscale = transform.resize(grayscale, (state_height, state_width))
     return grayscale
 
-def _CNN():
+def _loss(actions, logits, discounted_rewards):
+    cross_entropy = K.tf.nn.softmax_cross_entropy_with_logits_v2(labels=actions, logits=logits)
+    J = K.tf.reduce_mean(cross_entropy * discounted_rewards)
+    return J
+
+def _CNN(mode='train'):
     
     # CNN architecture: similar DeepMind's implementation for Atari games, except we do not downsample as aggressively
     # Input image:      84 x 84 x 4 (4 gray-scale images of 84 x 84 pixels). Input image is resized from (224, 320, 3)
@@ -51,24 +63,38 @@ def _CNN():
     # Conv layer 2:     32 filters 3 x 3, stride 1, relu.
     # Conv layer 3:     64 filters 3 x 3, stride 1, relu.
     # Fully-conn. 1:    512 units, relu.
-    # Fully-conn. 2:    256 units, sigmoid. (we used sigmoid here because the game seems to allow for simultaneous actions)
-    # for example, a possible action would be [0,1,1,0,1,1,1,0,0,0,1,0] 
+    # Fully-conn. 2:    256 units, softmax
     # Fully-conn. 2:    num-action units (12,)
-    # Given the output, we choose actions with probability > 0.5\
+    # Given the output, we choose actions with probability > 0.5
     # loss: sum(Advantage_func * log(logits))
     
-    model = Sequential()
-    model.add(Conv2D(filters=16, kernel_size=(3,3), padding='same', activation='relu',
-                     input_shape=(state_height, state_width, stack_size)))
-    model.add(Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu'))
-    model.add(Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu'))
-    model.add(BatchNormalization())
-    model.add(Flatten())
-    model.add(Dense(512, activation='relu'))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(12, activation='sigmoid'))
+    img = Input(shape=(state_height, state_width, stack_size))
+    yTrue = Input(shape=(12,))
+    r = Input(shape=(1,))
     
-    return model
+    conv = Conv2D(filters=16, kernel_size=(3,3), padding='same', activation='relu',)(img)
+    conv = MaxPooling2D()(conv)
+    conv = BatchNormalization()(conv)
+    conv = Conv2D(filters=32, kernel_size=(3,3), padding='same', activation='relu')(conv)
+    conv = MaxPooling2D()(conv)
+    conv = BatchNormalization()(conv)
+    conv = Conv2D(filters=64, kernel_size=(3,3), padding='same', activation='relu')(conv)
+    conv = MaxPooling2D()(conv)
+    conv = BatchNormalization()(conv)
+    l = Flatten()(conv)
+    l = Dense(512, activation='relu')(l)
+    l = Dense(256, activation='relu')(l)
+    yPred = Dense(12, activation='linear')(l)
+    
+    trainer = Model(inputs=[img, yTrue, r], outputs=yPred)
+    trainer.add_loss(_loss(yTrue, yPred, r))
+    adam = optimizers.Adam(lr=0.01)
+    trainer.compile(loss=None, optimizer=adam)
+    
+    if mode == 'train':
+        return trainer
+    else:
+        return Model(inputs=img, outputs=yPred)
 
 def _stack_frames(frames, new_state):
     # stack sequential frames together to detect motion
@@ -77,49 +103,48 @@ def _stack_frames(frames, new_state):
         frames = deque(frames, maxlen=stack_size)
         
     if len(frames) == 0: # having zero frames indicates that this is a new episode
-        for _ in range(frames):
+        for _ in range(stack_size):
             frames.append(new_state) # initialize the deque with 4 copies of the first state
-        stacked_states = np.stack(frames, axis=2) 
-    
-    frames.append(new_state)
-    stacked_states = np.stack(frames, axis=2)
-    
-    return stacked_states, frames
+    else:
+        frames.append(new_state)
+        
+    return frames
 
 class Agent():
-    def __init__(self, training, level, render=False):
-        self.env = retro.make(game='Airstriker-Genesis', state=level)
-        self.training = training
-        self.model = _CNN()
-    
-    def train(self, state, action):
-        return
-    
-    def predict(self, state): # predicts an action given state
-        return
+    def __init__(self, game, level):
+        self.env = retro.make(game, level)
+        self.trainer = _CNN(mode='train')          
+        self.predictor = _CNN(mode='predict')
+            
+    def train(self, states, actions, discounted_rewards):
+        self.trainer.fit([states, actions, discounted_rewards], batch_size=16, epochs=1)
     
     def preprocess(self, state): # convert to grayscale
         state = _preprocess(state)
         return state
     
     def stack_frames(self, frames, new_state):
-        stacked_states = _stack_frames(frames, new_state)
-        return stacked_states, new_state
-
-    def log(self):
-        # log (for results/discussion)
-        # - episode number
-        # - state_num
-        # - life_remain
-        # - episode_reward
-        # - loss
-        return
+        frames = _stack_frames(frames, new_state)
+        return frames
     
-        
-
-
-
-
-
-
-
+    def get_action(self, state):
+        logits = self.predictor.predict(state.reshape(1,state_height,state_width,stack_size))
+        logits = softmax(logits)
+        logits = logits.squeeze() # from (1, 12) to (12,)
+        action = np.zeros(logits.shape[0])
+        idx = np.random.choice(logits.shape[0], p=logits) # sample action from probability distribution  
+        action[idx] = 1
+        return action
+    
+    def perform_action(self, action, render=False):
+        observation, reward, done, info = self.env.step(action)
+        if render == True:
+            self.env.render()
+        return observation, reward, done, info
+    
+    def reset(self):
+        state = self.env.reset()
+        return state
+    
+    def save(self):
+        self.trainer.save('./model/agent.h5')
--- train.py
+++ train.py
@@ -1,11 +1,15 @@
+import agent
+import numpy as np
+
 # train with openAI gym retro
 
 # =============================================================================
 # pseudo code:
-#     Create the NN maxReward = 0
+#     Create the NN 
+#     maxReward = 0
 #     Keep track of maximum reward 
 #     For episode_num in range(max_episodes): # max_episodes is the rollout
-#         episode + 1 reset environment reset stores (states, actions, rewards) 
+#         episode + 1; reset environment; reset stores (states, actions, rewards) 
 #         For each step: 
 #             Choose action a 
 #             Perform action a - always instruct aircraft to shoot; train maneuver only 
@@ -15,3 +19,89 @@
 #                 Calculate discounted episode reward (discount_factor * reward)
 #                 Optimize
 # =============================================================================
+
+max_episodes = 1000
+gamma = 0.98
+state_width = 84 # resized state width
+state_height = 84 # resized state height
+stack_size = 4 # how many states/frames to stack together to detect motion
+frames = []
+
+player = agent.Agent('Airstriker-Genesis', 'Level1')
+
+# variables to log
+with open('log_reward.txt', mode='a', buffering=1) as f:
+    msg = 'episode_num\tmean_reward\ttotal_reward\n'
+    f.write(msg)
+print('Begin roll out...')
+
+for episode_num in range(max_episodes): # or let it run indefinitely (?)
+    
+    frames= []
+    all_states = []
+    all_actions = []
+    rewards = []
+    total_reward = 0
+    lives = 3
+    done = False
+    state = player.reset()
+    old_frames = None
+    
+    while not done:
+        state = player.preprocess(state)
+        frames = player.stack_frames(frames, state)
+        stacked_states = np.stack(frames, axis=2) # dimension of stacked_states: (84, 84, 4); stacked frames necessary to detect motion
+        action = player.get_action(stacked_states) # get an action given state
+        new_state, r, done, info = player.perform_action(action)
+        total_reward += r
+        all_states.append(stacked_states)
+        all_actions.append(action)
+        state = new_state
+        
+        # clip the rewards and penalize if agent loses a life
+        if info['lives'] == lives:
+            if r > 0:
+                rewards.append(1.0)
+            else:
+                rewards.append(0.0)
+    
+        if info['lives'] < lives:
+            rewards.append(-1.0)
+            lives = info['lives']
+        
+        if done:
+            # record episode number and the mean reward for this episode
+            with open('log_reward.txt', mode='a', buffering=1) as f:
+                mean_reward = total_reward / len(rewards)
+                msg = '{0}\t{1:.2f}\t{2}\n'.format(episode_num+1, mean_reward, total_reward)
+                print('Episode: {0}\tMean Reward: {1:.2f}\tTotal Reward: {2}\n'.format(episode_num+1, mean_reward, total_reward))
+                f.write(msg)
+                
+            episode_states = np.array(all_states)
+            episode_actions = np.vstack(np.array(all_actions))
+            episode_rewards = np.vstack(np.array(rewards))
+            episode_rewards = agent.discount(episode_rewards, gamma)
+            
+            # error occurs when the episode plays for too long; not sure why
+            # clip the states, actions and rewards if the episode exceeds 3000 states
+            if len(episode_states) > 3000:
+                episode_states = episode_states[:3000]
+                episode_actions = episode_actions[:3000]
+                episode_rewards = episode_rewards[:3000]
+            player.train(episode_states, episode_actions, episode_rewards)
+
+# Model summary        
+# =============================================================================
+#             print(episode_states.shape) # (num_states, 84, 84, 4)
+#             print(episode_actions.shape) # (num_states, 12)
+#             print(episode_rewards.shape) # (num_states, 1)
+#             print(player.model.summary()) 
+# =============================================================================
+        
+    # save weights once in a while
+    if episode_num % 100 == 0:
+        player.save()
+        
+    
+    
+    
